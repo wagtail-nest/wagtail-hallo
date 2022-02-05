450edf36c01e961b97ddc79ad763952817da4b7f
LB Johnston <lb.johnston@virginaustralia.com>
2022-02-05 14:27:24 +1000

    add initial test models & template

---
+++ wagtail_legacy_hallo_editor/test/testapp/models.py
@@ -0,0 +1,18 @@
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.models import Page
+
+class HalloTestPage(Page):
+    body = RichTextField(editor='legacy', blank=True)
+
+    body_stream = StreamField([
+        ('heading', blocks.CharBlock(form_classname="full title")),
+        ('paragraph', blocks.RichTextBlock(editor='legacy')),
+    ])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('body', classname='full'),
+        StreamFieldPanel('body_stream'),
+    ]
