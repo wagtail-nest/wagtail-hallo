7391a8595df6dd4badb3245ec2147b266d3fb737
Robert Rollins <rrollins@caltech.edu>
2026-02-10 11:52:24 -0800

    Two fixes that Black insisted upon

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from setuptools import find_packages, setup
 
 from wagtail_hallo import __version__
 
-
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
